9960db6de52578a30cb02764b7509c6b74c275fb
baolin.liu <baolin.liu@jcipn.com>
2022-02-21 17:54:51 +0800

    update deliver.sh

--- deliver.sh
+++ deliver.sh
@@ -2,8 +2,8 @@ echo 'The following command runs and outputs the execution of your Java'
 echo 'application (which Jenkins built using Maven) to the Jenkins UI.'
 
 service_name="hello-blog"
-$service_prot=9000
+service_prot=9000
 
 set -x
-docker build -t $service_name
+docker build . -t $service_name
 docker run -d --name $service_name --net=host -p $service_prot:$service_prot $service_name
